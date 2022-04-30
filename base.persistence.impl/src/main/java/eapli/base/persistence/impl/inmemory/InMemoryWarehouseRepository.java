7e127248ae9a148cb49afdc88b37567ba641f695
Tiago Ferreira <1200601@isep.ipp.pt>
2022-04-30 15:55:04 +0100

    LAPR4-4 #time 10m #comment a possible ProductManagementSmokeTest

--- base.persistence.impl/src/main/java/eapli/base/persistence/impl/inmemory/InMemoryWarehouseRepository.java
+++ base.persistence.impl/src/main/java/eapli/base/persistence/impl/inmemory/InMemoryWarehouseRepository.java
@@ -1,5 +1,6 @@
 package eapli.base.persistence.impl.inmemory;
 
+import eapli.base.warehousemanagement.domain.AGVDock;
 import eapli.base.warehousemanagement.domain.Shelf;
 import eapli.base.warehousemanagement.domain.Warehouse;
 import eapli.base.warehousemanagement.domain.WarehouseName;
@@ -44,6 +45,16 @@ public class InMemoryWarehouseRepository extends InMemoryRepository<Warehouse, L
     @Override
     public void updateShelf(Shelf s) {
 
+    }
+
+    @Override
+    public Iterable<AGVDock> findAllAGVDock() {
+        return null;
+    }
+
+    @Override
+    public AGVDock searchAGVDockById(Integer id) {
+        return null;
     }
 
     @Override
