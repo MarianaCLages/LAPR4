0215f0beb3f7b547e2d1c9adcdcd0242b327367a
Miguel Jordão <1201487@isep.ipp.pt>
2022-05-24 02:13:43 +0100

    LAPR4-146 #time 40m #comment HTTPS Server configuration (configuration fixes)

--- base.app.server/src/main/java/eapli/base/httpsServer/HttpChatConsumer.java
+++ base.app.server/src/main/java/eapli/base/httpsServer/HttpChatConsumer.java
@@ -1,9 +1,12 @@
 package eapli.base.httpsServer;
 
+import eapli.base.httpsServer.domain.HTTPmessage;
+import eapli.base.httpsServer.domain.MessageFetcher;
+
 import java.io.*;
 import java.net.*; 
 
-class HttpChatConsumer {    
+public class HttpChatConsumer {
 	private static InetAddress serverIP;
 	private static int serverPort;
 	public static boolean userExit;
@@ -91,86 +94,3 @@ class HttpChatConsumer {
 
 
 } // HttpChatConsumer CLASS
-
-
-
-class MessageFetcher extends Thread {    
-	private	Socket TCPconn;
-	private InetAddress serverIP;
-	private int serverPort;
-	private int nextMessage;
-
-	public MessageFetcher(InetAddress ip, int port) { serverIP=ip; serverPort=port; }
-
-	public void run() {
-		nextMessage=0;
-		while(!HttpChatConsumer.userExit) {
-			String message = getNextMessage();
-			if(HttpChatConsumer.userExit) return;
-			if(message==null) {
-				System.out.println("Server not responding ...");
-				nextMessage=0;
-				try {Thread.sleep(5000); }
-				catch(InterruptedException ie) {
-					System.out.println("Thread Interrupted ...");
-				}
-			}
-			else {
-				System.out.println(message);
-				nextMessage++;
-			}
-		}
-	}
-	
-
-	private String getNextMessage() {
-		DataOutputStream sOut;
-		DataInputStream sIn;
-		try {   
-			synchronized (serverIP) { TCPconn = new Socket(serverIP, serverPort);}
-		}
-		catch(IOException ex) {
-			synchronized (serverIP) { TCPconn = null;}
-			return null;
-		}
-
-		HTTPmessage response;
-		try {
-			sOut = new DataOutputStream(TCPconn.getOutputStream());
-			sIn = new DataInputStream(TCPconn.getInputStream());
-			HTTPmessage request = new HTTPmessage();
-			request.setRequestMethod("GET");
-			request.setURI("/messages/" + nextMessage);
-			request.send(sOut);
-			response = new HTTPmessage(sIn);	// the server may hold the response
-			if(!response.getStatus().startsWith("200")) {
-				throw new IOException();
-			}
-
-		}
-		catch(IOException ex) {
-			synchronized (serverIP) { 
-				try { TCPconn.close(); } catch(IOException ex2) { }
-				TCPconn = null;
-			}
-			return null;
-		}
-		synchronized (serverIP) { 
-			try { TCPconn.close(); } catch(IOException ex2) { }
-			TCPconn = null;
-		}
-		return(new String(response.getContent()));
-	}
-
-	public void abort() { // close the socket to force the thread's exit
-		synchronized (serverIP) { 
-			if(TCPconn==null) return;
-			try { TCPconn.close(); } catch(IOException ex2) { }
-		}
-	}
-
-} // MessageFetcher CLASS
-
-
-
-
