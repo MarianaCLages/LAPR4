7f1e9065d5d907e9f07168d05af1b7e4fa4581c0
Eduardo Sousa <1200920@isep.ipp.pt>
2022-05-26 23:14:57 +0100

    LAPR4-146 #comment correction on the generation of the matrix

--- base.core/src/main/java/eapli/base/warehousemanagement/domain/Warehouse.java
+++ base.core/src/main/java/eapli/base/warehousemanagement/domain/Warehouse.java
@@ -64,32 +64,58 @@ public class Warehouse implements AggregateRoot<Long> {
     }
 
     public String[][] generatePlant() {
-        this.plant = new String[length + 1][width + 1];
+        int matrixLength = length / square;
+        int matrixWidth = width / square;
+        this.plant = new String[matrixLength + 1][matrixWidth + 1];
 
         // Initialize the plant
-        for (int i = 0; i < length + 1; i++) {
-            for (int j = 0; j < width + 1; j++) {
+        for (int i = 0; i < matrixLength + 1; i++) {
+            for (int j = 0; j < matrixWidth + 1; j++) {
                 this.plant[i][j] = "|  |";
             }
         }
-        System.out.println("Plant generated");
-
-        //TODO: generate the plant
 
         for (Aisle a : aisles) {
             plant[(int) a.begin().lSquare()][(int) a.begin().wSquare()] = "|A" + a.identification() + "|";
             plant[(int) a.end().lSquare()][(int) a.end().wSquare()] = "|A" + a.identification() + "|";
             plant[(int) a.depth().lSquare()][(int) a.depth().wSquare()] = "|A" + a.identification() + "|";
 
-            //fill the space between
-            for (int i = (int) a.begin().lSquare() + 1; i < a.end().lSquare(); i++) {
-                plant[i][(int) a.begin().wSquare()] = "|A" + a.identification() + "|";
+
+            //fill the space beteween the begin and end/depth
+            if (a.begin().wSquare() > a.depth().wSquare()) {
+                for (int i = (int) a.depth().wSquare(); i <= a.begin().wSquare(); i++) {
+                    for (int j = (int) a.begin().lSquare(); j < a.end().lSquare(); j++) {
+                        this.plant[j][i] = "|A" + a.identification() + "|";
+                    }
+                }
+            } else {
+                for (int i = (int) a.begin().wSquare(); i <= a.depth().wSquare(); i++) {
+                    for (int j = (int) a.begin().lSquare(); j <= a.end().lSquare(); j++) {
+                        this.plant[j][i] = "|A" + a.identification() + "|";
+                    }
+                }
+
             }
+
+            for (int i = (int) a.begin().wSquare(); i <= a.depth().wSquare(); i++) {
+                for (int j = (int) a.begin().lSquare(); j < a.end().lSquare(); j++) {
+                    this.plant[j][i] = "|A" + a.identification() + "|";
+                }
+            }
+
 
             for (Row r : a.rows()) {
                 plant[(int) r.begin().lSquare()][(int) r.begin().wSquare()] = "|R" + r.identification() + "|";
                 if (r.end().lSquare() != r.begin().lSquare()) {
-                    plant[(int) r.end().lSquare()][(int) r.end().wSquare()] = "|R" + r.identification() + "|";
+                    plant[((int) r.end().lSquare())][((int) r.end().wSquare())] = "|R" + r.identification() + "|";
+                }
+
+                //fill the space between
+                for (int i = ((int) r.begin().wSquare() + 1); i < r.end().wSquare(); i++) {
+                    plant[(int) r.begin().lSquare()][i] = "|R" + r.identification() + "|";
+                }
+                for (int i = ((int) r.begin().lSquare() + 1); i < r.end().lSquare(); i++) {
+                    plant[i][(int) r.begin().wSquare()] = "|R" + r.identification() + "|";
                 }
             }
 
@@ -98,26 +124,28 @@ public class Warehouse implements AggregateRoot<Long> {
 
         //add the agv docks
         for (AGVDock dock : agvDocks) {
-            plant[(int) dock.begin().lSquare()][(int) dock.begin().wSquare()] = "|D" + dock.dockDesignation() + "|";
-            plant[(int) dock.end().lSquare()][(int) dock.end().wSquare()] = "|D" + dock.dockDesignation() + "|";
-            plant[(int) dock.depth().lSquare()][(int) dock.depth().wSquare()] = "|D" + dock.dockDesignation() + "|";
+            plant[((int) dock.begin().lSquare())][((int) dock.begin().wSquare())] = "|" + dock.dockDesignation() + "|";
+            plant[((int) dock.end().lSquare())][((int) dock.end().wSquare())] = "|" + dock.dockDesignation() + "|";
+            plant[((int) dock.depth().lSquare())][((int) dock.depth().wSquare())] = "|" + dock.dockDesignation() + "|";
 
             //fill the space between
-            for (int i = (int) dock.begin().lSquare() + 1; i < dock.end().lSquare(); i++) {
-                plant[i][(int) dock.begin().wSquare()] = "|D" + dock.dockDesignation() + "|";
+            for (int i = ((int) dock.begin().lSquare() + 1); i < (dock.end().lSquare()); i++) {
+                plant[i][((int) dock.begin().wSquare())] = "|D" + dock.dockDesignation() + "|";
             }
         }
 
-   /*     //print the plant
-        for (int i = 0; i < length; i++) {
-            for (int j = 0; j < width; j++) {
+        //print the plant
+        for (int i = 0; i < matrixLength; i++) {
+            for (int j = 0; j < matrixWidth; j++) {
                 System.out.print(plant[i][j] + "");
             }
             System.out.println();
-        }*/
+        }
 
         return plant;
+
     }
+
 
     protected List<Aisle> aisles() {
         return this.aisles;
