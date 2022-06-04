6eaeddb5450e6692b3a9ddd7bb41693df3acc324
Tiago Ferreira <1200601@isep.ipp.pt>
2022-06-04 18:08:53 +0100

    LAPR4-165 #time 30m #comment SSL on the AGVManager implemmented

--- base.app.server/src/main/java/eapli/base/tcpServer/agvManager/presentation/TcpAgvSrv.java
+++ base.app.server/src/main/java/eapli/base/tcpServer/agvManager/presentation/TcpAgvSrv.java
@@ -5,6 +5,8 @@ import eapli.base.tcpServer.agvManager.domain.TcpAGVSrvThread;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import javax.net.ssl.SSLServerSocket;
+import javax.net.ssl.SSLServerSocketFactory;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -14,25 +16,42 @@ import java.util.List;
 import java.util.concurrent.Semaphore;
 
 public class TcpAgvSrv {
-    private static ServerSocket sock;
+    // private static ServerSocket sock;
+    private static SSLServerSocket sock = null;
     private static final Logger LOGGER = LogManager.getLogger(TcpAgvSrv.class);
 
-    public static void serverRun(int serverSockNum) throws IOException {
-        Socket cliSock;
+    private static final String TRUSTED_STORE = "base.app.server/src/main/java/eapli/base/tcpServer/agvManager/domain/TLS_AGV/agvServer_J.jks";
+    private static final String KEYSTORE_PASS = "forgotten";
 
+
+    public static void serverRun(int serverSockNum) throws IOException {
+        //SSLServerSocket sock = null;
+        Socket cliSock;
         Semaphore semOrder = new Semaphore(0);
         Semaphore semAGV = new Semaphore(0);
         List<String> orders = Collections.synchronizedList(new LinkedList<>());
         List<String> agvs = Collections.synchronizedList(new LinkedList<>());
 
+        // Trust these certificates provided by authorized clients
+        System.setProperty("javax.net.ssl.trustStore", TRUSTED_STORE);
+        System.setProperty("javax.net.ssl.trustStorePassword", KEYSTORE_PASS);
+
+        // Use this certificate and private key as server certificate
+        System.setProperty("javax.net.ssl.keyStore", TRUSTED_STORE);
+        System.setProperty("javax.net.ssl.keyStorePassword", KEYSTORE_PASS);
+
+        SSLServerSocketFactory sslF = (SSLServerSocketFactory) SSLServerSocketFactory.getDefault();
+
+
         try {
-            sock = new ServerSocket(serverSockNum);
+            sock = (SSLServerSocket) sslF.createServerSocket(serverSockNum);
+            sock.setNeedClientAuth(true);
             LOGGER.info("Server socket created");
             FifoAGVTwin fifoAGVTwin = new FifoAGVTwin(semOrder, semAGV, orders, agvs);
             fifoAGVTwin.start();
         } catch (IOException ex) {
             sock.close();
-            LOGGER.error("Failed to open the order server socket");
+            LOGGER.error("Failed to open the agv server socket");
             ex.printStackTrace();
             System.exit(1);
         }
