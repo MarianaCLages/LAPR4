54852361db8d5286d513dd2c9d32a910bcebb801
Miguel Jordão <1201487@isep.ipp.pt>
2022-06-13 14:42:17 +0100

    LAPR4-207 #time 1h #comment Simulation Engine Module Update (Added AGV direction calculation) and Sensor Module update

--- base.app.agvtwin/src/sensors.c
+++ base.app.agvtwin/src/sensors.c
@@ -27,20 +27,20 @@ void* sensor_thread (void *arg) {
 	sensorAgvInfo st = *((sensorAgvInfo*) arg);
 	
 	if(st.sensorValue == 1) {
-	
-		//COMO É QUE VOU BUSCAR A DIREÇÃO DO AGV?
-		//Por agora vou considerar que se a variável que diz se o AGV se dirige para o obstáculo é 1 e se não é 0
 		
+		//Se a direção do AGV for 1, significa que ele esta neste momento em direção do obstáculo, se for 0, quer dizer que não está na direção desse obstáculo
+		
+		//Duvidas a tratar
 		//Se mandarmos um sinal para o módulo de POSITIONING, como é que sabemos o PID desse processo que está a correr esse módulo?
 		//E como é que sabemos o AGV que se trata? O mesmo está a ser passado para este módulo através da estrutura sensorAgvInfo e tem um PID específico
 		
-		if(direction == 1) {
+		if(st.direction == 1) {
 			//PARAR O AGV!
 			//mandar o USR1 para o modulo do POSITIONING
 		}
 	
 	} else if(st.sensorValue == 2) {
-		if(direction == 1) {
+		if(st.direction == 1) {
 			//Reduzir velocidade para metade!
 			//mandar o USR2 para o modulo do POSITIONING
 		}
--- base.app.agvtwin/src/simulationEngine.c
+++ base.app.agvtwin/src/simulationEngine.c
@@ -22,7 +22,11 @@
 
 //SIMULATION ENGINE MODULE
 
+//VERIFICAR DIREÇÕES
+//VERIFICAR RECONHECIMENTO DA PROXIMA POSIÇÃO
+
 pthread_mutex_t mux;
+pthread_mutex_t muxPlant;
 
 int main(void) {
 	
@@ -40,7 +44,7 @@ int main(void) {
 	int nAgvs = shm->numAgvs;
 	
 	//threads information
-	pthread_t threads[size];
+	pthread_t threads[nAgvs];
 	
 	for(i = 0 ; i < nAgvs ; i++) {
 		pthread_create(&threads[i], NULL, simulation_engine_thread, (void*) &shm);
@@ -52,6 +56,7 @@ int main(void) {
 	
 	//DEVE SER LANÇADO X THREADS CONSOANTE O NÚMERO DE AGVS A TRABALHAR!
 	//FAZER ALGO PARA TERMINAR AS THREADS QUANDO O AGV ACABA A ROUTE!
+	//PODEMOS FAZER AQUI UM HANDLER DE SINAL QUE ACABA TODAS AS THREADS EM EXECUÇÃO, QUE ACHAM?
 	
 }
 
@@ -68,20 +73,45 @@ void* simulation_engine_thread (void *arg) {
 	
 	for(;;) {
 	
+		//NEXT POSITION
 		int nx = shm.infoAgvs[currIndex].nextPosition.x;
 		int ny = shm.infoAgvs[currIndex].nextPosition.y;
 	
+		//CURRENT POSITION
 		int cx = shm.infoAgvs[currIndex].currentPosition.x;
 		int cy = shm.infoAgvs[currIndex].currentPosition.y;
-	
+		
+		//CURRENT VELOCITY
+		int vx = shm.infoAgvs[currIndex].vInfo.x;
+		int vy = shm.infoAgvs[currIndex].vInfo.y;
+		
 		if(shm.plant[nx][ny] == 1) {
 	
 			//MUDAR VALOR SENSORES
 			//TALVEZ SEMPRE QUE É ALTERADO UM VALOR DO SENSOR LANÇAR 1 THREAD, DESSE MESMO SENSOR, UMA THREAD QUE EXECUTE O SENSOR MODULE E QUE VERIFIQUE O SENTIDO/VELOCIDADE DE UM AGV? E SE FOR NECESSÁRIO LANÇAR UM SINAL PARA FAZER UMA NOVA ROUTE?
 		
 			if(cx == nx) { //CIMA OU BAIXO
-				if(cy < ny) shm.infoAgvs[currIndex].sInfo.back = 1; //BAIXO
-				else if(cy > ny) shm.infoAgvs[currIndex].sInfo.front = 1; //CIMA
+				if(cy < ny) {//BAIXO
+					shm.infoAgvs[currIndex].sInfo.back = 1; 
+					
+					if((vx == 0) && (vy > 0)) //DIREÇÃO -> 1
+					else //DIREÇÃO -> 0
+					
+					//CRIAR THREAD SENSOR
+					//TALVEZ APENAS CRIAR A THREAD CASO A DIREÇÃO SEJA 1!
+				
+				}
+				
+				else if(cy > ny) { //CIMA
+					shm.infoAgvs[currIndex].sInfo.front = 1;
+					
+					if((vx == 0) && (vy < 0)) //DIREÇÃO -> 1
+					else //DIREÇÃO -> 0
+					
+					//CRIAR THREAD SENSOR
+					//TALVEZ APENAS CRIAR A THREAD CASO A DIREÇÃO SEJA 1!
+					
+				} 
 				
 				//ESQUERDA DIREIRA
 				shm.infoAgvs[currIndex].sInfo.right = 0;
@@ -94,12 +124,34 @@ void* simulation_engine_thread (void *arg) {
 				shm.infoAgvs[currIndex].sInfo.backLeft = 0;
 				
 				//VERIFICAR QUE A POSIÇÃO CONTINUA OCUPADA!
+				
+				pthread_mutex_lock(&muxPlant);
 				shm.plant[cx][cy] = 1;
+				pthread_mutex_unlock(&muxPlant);
 			
 			} else if(cy == ny) { //ESQUERDA OU DIREITA
-				if(cx < nx) shm.infoAgvs[currIndex].sInfo.right = 1; //DIREITA
-				else if(cx > nx) shm.infoAgvs[currIndex].sInfo.left = 1; //ESQUERDA
+				if(cx < nx) { //DIREITA
+					shm.infoAgvs[currIndex].sInfo.right = 1; 
+					
+					if((vx > 0) && (vy == 0)) //DIREÇÃO -> 1
+					else //DIREÇÃO -> 0
+					
+					//CRIAR THREAD SENSOR
+					//TALVEZ APENAS CRIAR A THREAD CASO A DIREÇÃO SEJA 1!
+					
+				}
 				
+				else if(cx > nx) { //ESQUERDA
+					shm.infoAgvs[currIndex].sInfo.left = 1;
+					
+					if((vx < 0) && (vy == 0)) //DIREÇÃO -> 1
+					else //DIREÇÃO -> 0
+					
+					//CRIAR THREAD SENSOR
+					//TALVEZ APENAS CRIAR A THREAD CASO A DIREÇÃO SEJA 1!
+					
+				}
+					 
 				//CIMA OU BAIXO
 				shm.infoAgvs[currIndex].sInfo.back = 0;
 				shm.infoAgvs[currIndex].sInfo.front = 0;
@@ -111,14 +163,54 @@ void* simulation_engine_thread (void *arg) {
 				shm.infoAgvs[currIndex].sInfo.backLeft = 0;
 				
 				//VERIFICAR QUE A POSIÇÃO CONTINUA OCUPADA!
+				pthread_mutex_lock(&muxPlant);
 				shm.plant[cx][cy] = 1;
+				pthread_mutex_unlock(&muxPlant);
 			
 			} else if(cy != ny && cx != nx){ //OBLIQUO
 			
-				if(cx < nx && cy > ny) shm.infoAgvs[currIndex].sInfo.frontRight = 1; //CIMA DIREITA
-				else if(cx < nx && cy < ny) shm.infoAgvs[currIndex].sInfo.backRight = 1; //BAIXO DIREITA
-				else if(cx > nx && cy > ny) shm.infoAgvs[currIndex].sInfo.frontLeft = 1; //CIMA ESQUERDA
-				else if(cx > nx && cy < ny) shm.infoAgvs[currIndex].sInfo.backLeft = 1; //BAIXO ESQUERDA
+				if(cx < nx && cy > ny){  //CIMA DIREITA
+					 shm.infoAgvs[currIndex].sInfo.frontRight = 1;
+					 
+					 if((vx > 0) && (vy < 0)) //DIREÇÃO -> 1
+					 else //DIREÇÃO -> 0
+					
+					//CRIAR THREAD SENSOR
+					//TALVEZ APENAS CRIAR A THREAD CASO A DIREÇÃO SEJA 1!
+					 
+				}
+				
+				else if(cx < nx && cy < ny) { //BAIXO DIREITA
+					 shm.infoAgvs[currIndex].sInfo.backRight = 1;
+					 
+					 if((vx > 0) && (vy > 0)) //DIREÇÃO -> 1
+					 else //DIREÇÃO -> 0
+					
+					//CRIAR THREAD SENSOR
+					//TALVEZ APENAS CRIAR A THREAD CASO A DIREÇÃO SEJA 1!
+					 
+				}
+				
+				else if(cx > nx && cy > ny) { //CIMA ESQUERDA
+					 shm.infoAgvs[currIndex].sInfo.frontLeft = 1;
+					 
+					 if((vx < 0) && (vy < 0)) //DIREÇÃO -> 1
+					 else //DIREÇÃO -> 0
+					
+					//CRIAR THREAD SENSOR
+					//TALVEZ APENAS CRIAR A THREAD CASO A DIREÇÃO SEJA 1!
+					 
+				}
+				else if(cx > nx && cy < ny) { //BAIXO ESQUERDA
+					 shm.infoAgvs[currIndex].sInfo.backLeft = 1;
+					 
+					 if((vx < 0) && (vy > 0)) //DIREÇÃO -> 1
+					 else //DIREÇÃO -> 0
+					
+					//CRIAR THREAD SENSOR
+					//TALVEZ APENAS CRIAR A THREAD CASO A DIREÇÃO SEJA 1!
+					 
+				} 
 		
 				//ESQUERDA DIREIRA
 				shm.infoAgvs[currIndex].sInfo.right = 0;
@@ -129,7 +221,9 @@ void* simulation_engine_thread (void *arg) {
 				shm.infoAgvs[currIndex].sInfo.front = 0;
 				
 				//VERIFICAR QUE A POSIÇÃO CONTINUA OCUPADA!
+				pthread_mutex_lock(&muxPlant);
 				shm.plant[cx][cy] = 1;
+				pthread_mutex_unlock(&muxPlant);
 				
 			}
 		
@@ -137,49 +231,110 @@ void* simulation_engine_thread (void *arg) {
 				
 			//LANÇAR ALGO PARA REDUZIR A VELOCIDADE CASO A DIREÇÃO DO AGV SEJA NO SENTIDO DESTE OBSTÁCULO
 				
-			if(ny == cy) shm.infoAgvs[currIndex].sInfo.right = 2;
-			else if(ny < cy) shm.infoAgvs[currIndex].sInfo.frontRight = 2;
-			else if(ny > cy) shm.infoAgvs[currIndex].sInfo.backRight = 2;
+			if(ny == cy) {
+				shm.infoAgvs[currIndex].sInfo.right = 2; //DIREITA
+				
+				if((vx > 0) && (vy == 0)) //DIREÇÃO -> 1
+				else //DIREÇÃO -> 0
+				
+			}
+			
+			else if(ny < cy) { //CIMA DIREITA
+				 shm.infoAgvs[currIndex].sInfo.frontRight = 2; 
+				 
+				 if((vx > 0) && (vy < 0)) //DIREÇÃO -> 1
+				 else //DIREÇÃO -> 0
+				 
+			}
+			 
+			else if(ny > cy) { //BAIXO DIREITA
+				 shm.infoAgvs[currIndex].sInfo.backRight = 2; 
+				 
+				 if((vx > 0) && (vy > 0)) //DIREÇÃO -> 1
+				 else //DIREÇÃO -> 0
+				 
+			}
 			
 			//ALTERAR VALOR DA PLANTA PARA INFORMAR QUE AQUELA POSIÇÃO ESTA OCUPADA!
 			
+			pthread_mutex_lock(&muxPlant);
 			shm.plant[nx][ny] = 1;
 			shm.plant[cx][cy] = 0;
+			pthread_mutex_unlock(&muxPlant);
 				
 		} else if(shm.plant[nx - 1][ny] == 1 && ((nx - 1) != cx)) { 
 				
 			//LANÇAR ALGO PARA REDUZIR A VELOCIDADE CASO A DIREÇÃO DO AGV SEJA NO SENTIDO DESTE OBSTÁCULO
 				
-			if(ny == cy) shm.infoAgvs[currIndex].sInfo.left = 2;
-			else if(ny < cy) shm.infoAgvs[currIndex].sInfo.frontLeft = 2;
-			else if(ny > cy) shm.infoAgvs[currIndex].sInfo.backLeft = 2;
+			if(ny == cy) { //ESQUERDA
+				shm.infoAgvs[currIndex].sInfo.left = 2;
+				
+				if((vx < 0) && (vy == 0)) //DIREÇÃO -> 1
+				else //DIREÇÃO -> 0
+				
+			}
+			
+			else if(ny < cy) { //CIMA ESQUERDA
+				shm.infoAgvs[currIndex].sInfo.frontLeft = 2;
+				
+				if((vx < 0) && (vy < 0)) //DIREÇÃO -> 1
+				else //DIREÇÃO -> 0
+				
+			}
+			
+			else if(ny > cy) { //BAIXO ESQUERDA
+				 shm.infoAgvs[currIndex].sInfo.backLeft = 2;
+				 
+				 if((vx < 0) && (vy > 0)) //DIREÇÃO -> 1
+				 else //DIREÇÃO -> 0
+				 
+			}
 			
 			//ALTERAR VALOR DA PLANTA PARA INFORMAR QUE AQUELA POSIÇÃO ESTA OCUPADA!
 			
+			pthread_mutex_lock(&muxPlant);
 			shm.plant[nx][ny] = 1;
 			shm.plant[cx][cy] = 0;
+			pthread_mutex_unlock(&muxPlant);
 				
 		} else if(shm.plant[nx][ny - 1] == 1 && ((ny - 1) != cy)) { 
 				
 			//LANÇAR ALGO PARA REDUZIR A VELOCIDADE CASO A DIREÇÃO DO AGV SEJA NO SENTIDO DESTE OBSTÁCULO
 				
-			if(nx == cx) shm.infoAgvs[currIndex].sInfo.front = 2;
+			if(nx == cx) { //CIMA
+				 shm.infoAgvs[currIndex].sInfo.front = 2;
+				 
+				 if((vx == 0) && (vy < 0)) //DIREÇÃO -> 1
+				 else //DIREÇÃO -> 0
+				 
+				 
+			}
 			
 			//ALTERAR VALOR DA PLANTA PARA INFORMAR QUE AQUELA POSIÇÃO ESTA OCUPADA!
 			
+			pthread_mutex_lock(&muxPlant);
 			shm.plant[nx][ny] = 1;
 			shm.plant[cx][cy] = 0;
+			pthread_mutex_unlock(&muxPlant);
 							
 		} else if(shm.plant[nx][ny + 1] == 1 && ((ny + 1) != cy)) { 
 				
 			//LANÇAR ALGO PARA REDUZIR A VELOCIDADE CASO A DIREÇÃO DO AGV SEJA NO SENTIDO DESTE OBSTÁCULO
 				
-			if(nx == cx) shm.infoAgvs[currIndex].sInfo.back = 2;
+			if(nx == cx) { //BAIXO
+				 shm.infoAgvs[currIndex].sInfo.back = 2;
+				 
+				 if((vx == 0) && (vy > 0)) //DIREÇÃO -> 1
+				 else //DIREÇÃO -> 0
+				 
+			 }
 			
 			//ALTERAR VALOR DA PLANTA PARA INFORMAR QUE AQUELA POSIÇÃO ESTA OCUPADA!
 			
+			pthread_mutex_lock(&muxPlant);
 			shm.plant[nx][ny] = 1;
 			shm.plant[cx][cy] = 0;
+			pthread_mutex_unlock(&muxPlant);
 								
 		} 
 		//DORMIR 100MS 
