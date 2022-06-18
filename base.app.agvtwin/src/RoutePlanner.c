32ee9bf862710472253e2fbf2937d6005de2f6bb
EduardoSousa <1200920@isep.ipp.pt>
2022-06-18 16:42:27 +0100

    LAPR4-166 RoutePlaner fixed

--- base.app.agvtwin/src/RoutePlanner.c
+++ base.app.agvtwin/src/RoutePlanner.c
@@ -1,244 +1,249 @@
 // Priority Queue implementation in C inspired by: https://www.programiz.com/dsa/priority-queue
 #include "geralHeader.h"
 
-int dp[50][50][2000];
-int dir[4][2] = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
-
 int size = 0;
-int ind = 0;
 
-void swap(cell *a, cell *b)
-{
-    cell temp = *b;
-    *b = *a;
-    *a = temp;
-}
+int rowD[] = {-1, 0, 0, 1};
+int colD[] = {0, -1, 1, 0};
 
-int isEqual(cell a, cell b)
+typedef struct
 {
-    return a.x == b.x && a.y == b.y;
-}
+    size_t head;
+    size_t tail;
+    size_t size;
+    void **data;
+} queue_t;
 
-int isGreater(cell a, cell b)
+void *queue_read(queue_t *queue)
 {
-    return a.x > b.x + a.y > b.y;
-}
-
-int isLess(cell a, cell b)
-{
-    return a.x < b.x + a.y < b.y;
-}
-
-// Function to heapify the tree
-void heapify(cell array[], int size, int i)
-{
-    if (size == 1)
+    if (queue->tail == queue->head)
     {
-        printf("Single element in the heap");
+        return NULL;
     }
-    else
-    {
-        // Find the largest among root, left child and right child
-        int largest = i;
-        int l = 2 * i + 1;
-        int r = 2 * i + 2;
-        if (l < size && isGreater(array[l], array[largest]))
-            largest = l;
-        if (r < size && isGreater(array[r], array[largest]))
-            largest = r;
-
-        // Swap and continue heapifying if root is not largest
-        if (largest != i)
-        {
-            swap(&array[i], &array[largest]);
-            heapify(array, size, largest);
-        }
-    }
+    void *handle = queue->data[queue->tail];
+    queue->data[queue->tail] = NULL;
+    queue->tail = (queue->tail + 1) % queue->size;
+    return handle;
 }
 
-// Function to insert an element into the tree
-void insert(cell array[], cell newNum)
+int queue_write(queue_t *queue, void *handle)
 {
-    if (size == 0)
-    {
-        array[0] = newNum;
-        size += 1;
-    }
-    else
-    {
-        array[size] = newNum;
-        size += 1;
-        for (int i = size / 2 - 1; i >= 0; i--)
-        {
-            heapify(array, size, i);
-        }
-    }
-}
-
-// Function to delete an element from the tree
-void deleteRoot(cell array[], cell num)
-{
-    int i;
-    for (i = 0; i < size; i++)
-    {
-        if (isEqual(num, array[i]))
-            break;
-    }
-
-    swap(&array[i], &array[size - 1]);
-    size -= 1;
-    for (int i = size / 2 - 1; i >= 0; i--)
-    {
-        heapify(array, size, i);
-    }
-}
-
-// Print the array
-void printArray(int array[], int size)
-{
-    for (int i = 0; i < size; ++i)
-        printf("%d ", array[i]);
-    printf("\n");
-}
-
-int compareCells(cell *a, cell *b)
-{
-    if (a->distance < b->distance)
+    if (((queue->head + 1) % queue->size) == queue->tail)
     {
         return -1;
     }
-    else if (a->distance > b->distance)
-    {
-        return 1;
-    }
-    else
-    {
-        return 0;
-    }
+    queue->data[queue->head] = handle;
+    queue->head = (queue->head + 1) % queue->size;
+    return 0;
 }
 
-int shortestPath(int *matrix, int rows, int column, int startX, int startY, int endX, int endY, cell *path)
+// the queue empty
+int queue_is_empty(queue_t *queue)
 {
-    // fill dp with int_max
-    for (int i = 0; i < column; i++)
+    return queue->tail == queue->head;
+}
+
+int isSafe(int *matrix, int *visited, int row, int col, int x, int y)
+{
+    return (x >= 0 && x < row && y >= 0 && y < col && matrix[x * col + y] == 0 && visited[x * col + y] == 0);
+}
+
+cell *bfs(int *matrix, int row, int col, cell initial)
+{
+    // set the visited array
+    int *visited = (int *)malloc(row * col * sizeof(int));
+    for (int i = 0; i < row * col; i++)
     {
-        for (int j = 0; j < rows; j++)
-        {
-            for (int k = 0; k < 2000; k++)
-            {
-                dp[i][j][k] = INT_MAX;
-            }
-        }
+        visited[i] = 0;
     }
-    
-    // Create a priority queue to store vertices
-    cell heap[rows * column];
-    cell c;
-    c.x = startX;
-    c.y = startY;
 
-    insert(heap, c);
-    
-    while (size > 0)
+    // set the queue
+    queue_t *queue = (queue_t *)malloc(sizeof(queue_t));
+    queue->head = 0;
+    queue->tail = 0;
+    queue->size = row * col;
+    queue->data = (void **)malloc(row * col * sizeof(void *));
+
+    // set the initial cell
+    cell *initialCell = (cell *)malloc(sizeof(cell));
+    initialCell->x = initial.x;
+    initialCell->y = initial.y;
+
+    // add the initial cell to the queue
+    queue_write(queue, initialCell);
+
+    // set the prev array
+    cell *prev = (cell *)malloc(row * col * sizeof(cell));
+
+    // while the queue is not empty
+    while (!queue_is_empty(queue))
     {
-        int x = heap[0].x;
-        int y = heap[0].y;
-        int distance = heap[0].distance;
+        // get the cell from the queue
+        cell *current = (cell *)queue_read(queue);
 
-        // prints the data
-
-        deleteRoot(heap, heap[0]);
-
-        if (x == endX && y == endY)
-        {
-            return distance;
-        }
-
-        distance++;
-
+        // for all the 4 directions
         for (int i = 0; i < 4; i++)
         {
+            // get the next cell
+            int x = current->x + rowD[i];
+            int y = current->y + colD[i];
 
-            int newX = x + dir[i][0];
-            int newY = y + dir[i][1];
-
-            if (newX == endX && newY == endY)
+            if (isSafe(matrix, visited, row, col, x, y))
             {
-                return distance;
-            }
-
-            if (newX >= 0 && newY >= 0 && newX < rows && newY < column && matrix[newX + newY * rows] == 0)
-            {
-
-                if (distance < dp[newX][newY][0])
-                {
-                    dp[newX][newY][0] = distance;
-                    c.x = newX;
-                    c.y = newY;
-                    c.distance = distance;
-                    path[distance] = c;
-                    insert(heap, c);
-                }
+                // add the next cell to the queue
+                cell *nextCell = (cell *)malloc(sizeof(cell));
+                nextCell->x = x;
+                nextCell->y = y;
+                queue_write(queue, nextCell);
+                // set as visited
+                visited[x * col + y] = 1;
+                // set the prev cell
+                prev[x * col + y].x = current->x;
+                prev[x * col + y].y = current->y;
             }
         }
     }
-    return -1;
+    return prev;
+}
+
+cell *reconstructPath(cell *prev, int nRows, int nCols, int startX, int startY, int endX, int endY)
+{
+    cell *path = (cell *)malloc((nRows * nCols) * sizeof(cell));
+    // reconstruct the path from the end to the start
+    int currentX = endX;
+    int currentY = endY;
+    int index = 0;
+    while (currentX != startX || currentY != startY)
+    {
+        cell *cell;
+        cell->x = currentX;
+        cell->y = currentY;
+        path[index] = *cell;
+        index++;
+        int recIndex = currentX * nCols + currentY;
+        currentX = prev[recIndex].x;
+        currentY = prev[recIndex].y;
+    }
+
+    // invert the path
+    cell *invertedPath = (cell *)malloc((nRows * nCols) * sizeof(cell));
+    int j = 0;
+    for (int i = index - 1; i >= 0; i--)
+    {
+        invertedPath[j] = path[i];
+        j++;
+    }
+
+    return invertedPath;
+}
+
+position *findShortestPath(int *matrix, int *visited, int rows, int column, int x, int y, int i, int j, int minDist, int distance, position *path)
+{
+
+    if (i == x && j == y)
+    {
+        if (distance < minDist)
+        {
+            minDist = distance;
+            return path;
+        }
+    }
+    // printf("\n\n\n\nNova posição: %d %d %d\n", i, j, distance);
+
+    // set as visited
+    visited[x * column + y] = 1;
+
+    // go to the bottom position
+    if (isSafe(matrix, visited, rows, column, x, y - 1))
+    {
+        // add to the path
+        position c;
+        c.x = i;
+        c.y = j;
+        path[distance] = c;
+        printf("\n\n\n\nNova posição: %d %d %d\n", i, j, distance);
+
+        return findShortestPath(matrix, visited, rows, column, x, y - 1, i, j, minDist, distance + 1, path);
+    }
+    // go to the right position
+    if (isSafe(matrix, visited, rows, column, x + 1, y))
+    {
+        // add to the path
+        position c;
+        c.x = i;
+        c.y = j;
+        path[distance] = c;
+        return findShortestPath(matrix, visited, rows, column, x + 1, y, i, j, minDist, distance + 1, path);
+    }
+    // go to the top position
+    if (isSafe(matrix, visited, rows, column, x, y + 1))
+    {
+        // add to the path
+        position c;
+        c.x = i;
+        c.y = j;
+        path[distance] = c;
+        return findShortestPath(matrix, visited, rows, column, x, y + 1, i, j, minDist, distance + 1, path);
+    }
+    // go to the left position
+    if (isSafe(matrix, visited, rows, column, x - 1, y))
+    {
+        // add to the path
+        position c;
+        c.x = i;
+        c.y = j;
+        path[distance] = c;
+        return findShortestPath(matrix, visited, rows, column, x - 1, y, i, j, minDist, distance + 1, path);
+    }
+
+    // backtrack: remove (i, j) from the visited matrix and path
+    // remove from the path
+
+    visited[x * column + y] = 0;
 }
 
 int calculateRoute(info *st)
 {
     int nRows = 21;
     int nCols = 19;
-   
-	size = 0;
-	ind = 0;	
-	
+
     int startX = st->currentPosition.x;
     int startY = st->currentPosition.y;
     int endX = st->destiny.x;
     int endY = st->destiny.y;
-    
-    cell routeInCell[nRows * nCols];
-    //resetDp();
-     
-    printf("Start X : %d\n",startX);
-    printf("Start Y : %d\n",startY);
-    printf("End X : %d\n",endX);
-    printf("End Y : %d\n",endY);
-    
-    int length = shortestPath(geralPlant, nRows, nCols, startX, startY, endX, endY, routeInCell);
 
-    printf("\nLength of the shortest path is %d\n", length);
+    // prints the initial position and the final position
+    printf("\n\n\n\nInicial: %d %d\n", startX, startY);
+    printf("Final: %d %d\n", endX, endY);
+    // creates the cell
+    cell initial;
+    initial.x = startX;
+    initial.y = startY;
 
+    // print the geralPlant
+    for (int i = 0; i < nRows; i++)
+    {
+        for (int j = 0; j < nCols; j++)
+        {
+            printf("%d ", geralPlant[i * nCols + j]);
+        }
+        printf("\n");
+    }
 
-	if(length != -1) {
-	
-		printf("\nPath is:\n");
+    cell *prev = bfs(geralPlant, nRows, nCols, initial);
 
-		//prints the route
-		for (int i = 1; i < length; i++)
-		{
-			printf("(%d, %d) ", routeInCell[i].x, routeInCell[i].y);
-		}
-		
-		//FAZER CONDIÇÃO VERIFICAÇÃO QUE O AGV NAO CONSEGUE CHEGAR AO DESTINO PORQUE NAO TEM BATERIA SUFICIENTE
-		
-	} else {
-		printf("\nAGV with and ID of: %d can't reach the desired location!\n",st->agvId);
-	}
-	
-	return length;
-   
-}
+    // reconstruct the path
+    cell *path = reconstructPath(prev, nRows, nCols, startX, startY, endX, endY);
 
-void resetDp(){
-	for(int i = 0; i < 50; i++) {
-		for(int j = 0 ; j < 50; j++) {
-			for(int k = 0 ; k < 2000; k++) {
-				dp[i][j][k] = 0;
-			}
-		}
-	}
-	
-	
+    // print the path
+    printf("\n\n\n\nCaminho: \n");
+
+    int i = 0;
+    while (path[i].x != 0 && path[i].y != 0)
+    {
+        printf("%d %d\n", path[i].x, path[i].y);
+        i++;
+    }
+
+    return 0;
 }
