d0d32833864202dd8aedaa0debcd10108469ef71
Miguel Jordão <1201487@isep.ipp.pt>
2022-06-18 17:11:03 +0100

    LAPR4-209 #time 30m #comment US 5100 : Battery Monitor thread update

--- base.app.agvtwin/src/batteryMonitor_thread.c
+++ base.app.agvtwin/src/batteryMonitor_thread.c
@@ -8,15 +8,33 @@ void* batteryMonitor_thread (void *arg) {
 	int battAfter = 0;
 	
 	for(;;){
+		
 		sleep(4);
 		
-		battBefore = st->battery;
+		if(st->currentPosition.x == st->agvDock.x && st->currentPosition.y == st->agvDock.y){
+			if(battBefore != 100) {
+				battBefore = st->battery;
 		
-		battAfter = battBefore - 1;
+				battAfter = battBefore + 1;
 		
-		st->battery = battAfter;
-		printf("\nThe Battery of the AGV ID: %d has been updated!\nBattery -> (Before: %d, After: %d)\n\n",st->agvId,battBefore,battAfter);
+				st->battery = battAfter;
+				printf("\nAGV Recharging!\nThe Battery of the AGV ID: %d has been updated!\nBattery -> (Before: %d, After: %d)\n\n",st->agvId,battBefore,battAfter);
+			
+			} else {
+				printf("\nAGV Battery Full!\n\n");
+			}
+					
+		} else {
+			battBefore = st->battery;
+		
+			battAfter = battBefore - 1;
+		
+			st->battery = battAfter;
+			printf("\nAGV Moving!\nThe Battery of the AGV ID: %d has been updated!\nBattery -> (Before: %d, After: %d)\n\n",st->agvId,battBefore,battAfter);
 	
+			
+		}
+			
 	}
 
 	pthread_exit(NULL);
