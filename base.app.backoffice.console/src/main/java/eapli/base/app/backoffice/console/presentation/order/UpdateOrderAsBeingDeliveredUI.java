7e933f72de31bf68334a8eafbab7a3f3babe0cab
Tiago Ferreira <1200601@isep.ipp.pt>
2022-06-19 17:57:14 +0100

    LAPR4-168 #time 5m #comment Updates on the US1006 UI

--- base.app.backoffice.console/src/main/java/eapli/base/app/backoffice/console/presentation/order/UpdateOrderAsBeingDeliveredUI.java
+++ base.app.backoffice.console/src/main/java/eapli/base/app/backoffice/console/presentation/order/UpdateOrderAsBeingDeliveredUI.java
@@ -55,6 +55,7 @@ public class UpdateOrderAsBeingDeliveredUI extends AbstractUI {
                     System.out.println(showOrder(orderList));
                 }
 
+                exist = 0;
             }
         }
 
