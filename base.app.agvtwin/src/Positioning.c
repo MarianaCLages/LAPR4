f70c0f5e936475ab734ddaa79ccab126d41f889d
Miguel Jordão <1201487@isep.ipp.pt>
2022-06-19 20:14:35 +0100

    LAPR4-209 #time 20m #comment US5100 : Positioning Module update

--- base.app.agvtwin/src/Positioning.c
+++ base.app.agvtwin/src/Positioning.c
@@ -1,37 +1,42 @@
 #include "geralHeader.h"
 
+//POSITIONING MODULE
+
 void* position_thread(void *arg) {
 	
 	info* st = (info*) arg;
 	
+	//GETS AGV INITIAL INFORMATION
     st->currentPosition = st->route[0];
     st->nextPosition = st->route[1];
     int i = 0;
+    
+    //Starts the value by default
+    st->secsSleep = 1;
 
     while (i < (st->routeLength)) {
         // Update velocity
         st->vInfo.x = st->route[i].x - st->currentPosition.x;
         st->vInfo.y = st->route[i].y - st->currentPosition.y;
 
-        sleep(1);
+        sleep(st->secsSleep);
 
-        // Update position
+        // Update position (THIS PRINTS SERVE AS A LOG)
         printf("AGV with an ID of: %d: Moving to (%d, %d)\n", st->agvId, st->nextPosition.x, st->nextPosition.y);
         printf("AGV with an ID of: %d: Velocity direction -> (xDirection: %d, yDirection: %d)\n", st->agvId, st->vInfo.x, st->vInfo.y);
         
-        //MUTEX AQUI!
-        
-        if((st->currentPosition.x == st->agvDock.x) && (st->currentPosition.y == st->agvDock.y)) geralPlant[st->currentPosition.y * 19 + st->currentPosition.x] = 0;
+        // Update the current position to 0
+        geralPlant[st->currentPosition.y * 19 + st->currentPosition.x] = 0;
         
         i++;
         st->currentPosition = st->nextPosition;
         st->nextPosition = st->route[i];
         
-        //MUTEX AQUI!
+		//UPDATES THE PLANT, IN ORDER TO ALERT OTHER THREADS THAT THE CURRENT POSITION OF THE AGV IS IN THIS INDEX
         geralPlant[st->currentPosition.y * 19 + st->currentPosition.x] = 1;
     
 	}
-	    
+	
     pthread_exit(NULL);
 	
 }
