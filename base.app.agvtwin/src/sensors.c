22a08787861492e42a1ad736bab4fbbda117d9d4
Miguel Jordão <1201487@isep.ipp.pt>
2022-06-19 20:14:57 +0100

    LAPR4-209 #time 5m #comment US5100 : Sensors Update

--- base.app.agvtwin/src/sensors.c
+++ base.app.agvtwin/src/sensors.c
@@ -2,27 +2,22 @@
 
 //SENSORS MODULE
 
-void* sensor_thread (void *arg) {
+void* sensor_threadFunction (void *arg) {
 	
-	sensorAgvInfo st = *((sensorAgvInfo*) arg);
+	sensorAlert* var = (sensorAlert*) arg;
 	
-	if(st.sensorValue == 1) {
-		
+	if(var->sensorValue == 1) {
 		//Se a direção do AGV for 1, significa que ele esta neste momento em direção do obstáculo, se for 0, quer dizer que não está na direção desse obstáculo
 		
-		//Duvidas a tratar
-		//Se mandarmos um sinal para o módulo de POSITIONING, como é que sabemos o PID desse processo que está a correr esse módulo?
-		//E como é que sabemos o AGV que se trata? O mesmo está a ser passado para este módulo através da estrutura sensorAgvInfo e tem um PID específico
-		
-		if(st.direction == 1) {
-			//PARAR O AGV!
-			//mandar o USR1 para o modulo do POSITIONING
+		if(var->directionValue == 1) {
+			//Stop the AGV
+			var->st->secsSleep = 2;
 		}
 	
-	} else if(st.sensorValue == 2) {
-		if(st.direction == 1) {
-			//Reduzir velocidade para metade!
-			//mandar o USR2 para o modulo do POSITIONING
+	} else if(var->sensorValue == 2) {
+		if(var->directionValue == 1) {
+			//Reduce Velocity to half
+			var->st->secsSleep = 2;
 		}
 	
 	}
